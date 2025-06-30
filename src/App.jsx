9b5d6f44031e8fee6edc9f0d0860e1a4b8549853
Dominic Smart <dominicsmart99@gmail.com>
2025-06-29 23:30:08 -0400

    Add Spinner component and integrate loading state in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
 
 const App = () => {
   const [animeList, setAnimeList] = useState([]);
@@ -47,19 +48,29 @@ const App = () => {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
         <section className="all-movies">
-          <h2>Currently Airing</h2>
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-          {Array.isArray(animeList) &&
-            animeList.map((anime) => (
-              <div key={anime.mal_id}>
-                <h2>{anime.title}</h2>
-                <img
-                  src={anime.images.jpg.image_url}
-                  alt={anime.title}
-                  width={150}
-                />
-              </div>
-            ))}
+          <h2 className="mt-[40px]">Currently Airing</h2>
+          {isLoading && (
+            <div className="w-full flex justify-center">
+              <Spinner />
+            </div>
+          )}
+          {errorMessage && !isLoading && (
+            <p className="text-red-500">{errorMessage}</p>
+          )}
+          {Array.isArray(animeList) && !isLoading && (
+            <ul>
+              {animeList.map((anime) => (
+                <div key={anime.mal_id}>
+                  <h2>{anime.title}</h2>
+                  <img
+                    src={anime.images.jpg.image_url}
+                    alt={anime.title}
+                    width={150}
+                  />
+                </div>
+              ))}
+            </ul>
+          )}
         </section>
       </div>
     </main>
